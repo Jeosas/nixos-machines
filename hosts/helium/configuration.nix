a9d0aa7855225aa3ca7c5ba548382bbd85680906
Jeosas <jeanbaptiste@wintergerst.fr>
2024-04-05 20:16:21 +0200

    feat(helium): added usb automount

--- hosts/helium/configuration.nix
+++ hosts/helium/configuration.nix
@@ -118,6 +118,11 @@
   # backlight
   programs.light.enable = true;
 
+  # usb automount
+  services.devmon.enable = true;
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
+
   environment.systemPackages = with pkgs; [
     git
     vim
