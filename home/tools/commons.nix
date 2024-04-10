341d658593e787175dc90f00cc116a3571225456
Jeosas <jeanbaptiste@wintergerst.fr>
2024-04-10 22:38:35 +0200

    feat(ranger): added file icons and pre-setup image preview

--- home/tools/commons.nix
+++ home/tools/commons.nix
@@ -4,9 +4,32 @@
   home.packages = with pkgs; [
     just
     bat
-    ranger
     ripgrep
     btop
     ncpamixer
   ];
+
+  programs.ranger = {
+    enable = true;
+    settings = {
+      show_hidden = true;
+      use_preview_script = true;
+      # TODO: activate when 1.9.4 release
+      # preview_images = true;
+      # preview_images_method = "sixel";
+    };
+    extraConfig = ''
+      default_linemode devicons
+    '';
+    plugins = [
+      {
+        name = "ranger_devicons";
+        src = builtins.fetchGit {
+          url = "https://github.com/alexanderjeurissen/ranger_devicons.git";
+          rev = "ed718dd6a6d5d2c0f53cba8474c5ad96185057e9";
+        };
+      }
+    ];
+    extraPackages = with pkgs; [ imagemagick ];
+  };
 }
