a71ab310f15e25ab009ce3e798d65aa984059396
Jeosas <jeanbaptiste@wintergerst.fr>
2024-04-23 07:39:58 +0200

    refactor(neovim): removed unused configs

--- home/applications/neovim/config/lua/jeovim/treesitter.lua
+++ home/applications/neovim/config/lua/jeovim/treesitter.lua
@@ -2,11 +2,8 @@
 vim.treesitter.language.register("hcl", "nomad")
 
 require("nvim-treesitter.configs").setup({
-	-- ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
-		enable = true, -- false will disable the whole extension
-		-- disable = { "css" }, -- list of language that will be disabled
+		enable = true,
 	},
 	autopairs = {
 		enable = true,
