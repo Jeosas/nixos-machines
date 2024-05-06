2e3275b8c4640f6f2391ff4da262678963313ea2
Jeosas <jeanbaptiste@wintergerst.fr>
2024-05-06 14:58:11 +0200

    feat(helium): setup cachix cache

--- hosts/helium/configuration.nix
+++ hosts/helium/configuration.nix
@@ -42,6 +42,13 @@
     settings = {
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 
