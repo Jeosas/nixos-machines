1f5291230500c40bf52507da04c842f8b547c03d
Jeosas <jeanbaptiste@wintergerst.fr>
2024-06-28 22:15:32 +0200

    feat(helium): changed battery charge thresholds

--- hosts/helium/battery.nix
+++ hosts/helium/battery.nix
@@ -23,8 +23,8 @@
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
-      START_CHARGE_THRESH_BAT0 = 40;
-      STOP_CHARGE_THRESH_BAT0 = 95;
+      START_CHARGE_THRESH_BAT0 = 70;
+      STOP_CHARGE_THRESH_BAT0 = 90;
     };
   };
 }
