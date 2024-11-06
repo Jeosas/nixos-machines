b253abd6954ae9ad7259a8e85b6f35ff675f3034
Jeosas <jeanbaptiste@wintergerst.fr>
2024-11-06 15:30:40 +0100

    feat(hyprland): remove unused env var

--- modules/nixos/desktop/hyprland/default.nix
+++ modules/nixos/desktop/hyprland/default.nix
@@ -39,7 +39,6 @@ in
               XDG_SESSION_TYPE = "wayland";
               GBM_BACKEND = "nvidia-drm";
               __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-              WLR_NO_HARDWARE_CURSORS = "1";
             };
           };
 
