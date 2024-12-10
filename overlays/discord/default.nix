8b6ee47981f6e67bdca9d253485b88ae0df3cbb7
Jeosas <jeanbaptiste@wintergerst.fr>
2024-12-10 11:25:26 +0100

    remove unused overlays

--- overlays/discord/default.nix
+++
@@ -1,8 +0,0 @@
-{...}: final: prev: {
-  discord = prev.discord.overrideAttrs (_: {
-    src = builtins.fetchTarball {
-      url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-      sha256 = "1h3697dx4cxf32p5bfkl8y04xvn2w8sq14cjw925z6x04mb15z07";
-    };
-  });
-}
