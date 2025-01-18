6309527ad47b1de8c610c6869e5a3d3152b04296
Jeosas <jeanbaptiste@wintergerst.fr>
2025-01-18 14:54:21 +0100

    fix helium config

--- systems/x86_64-linux/helium/default.nix
+++ systems/x86_64-linux/helium/default.nix
@@ -38,58 +38,17 @@ with lib.${namespace};
           ",preferred,auto,1"
         ];
       };
+    };
+
+    system.keyd = enabled;
+  };
+
+  home-manager.users.${config.${namespace}.user.name}.${namespace} = {
+    desktop = {
       addons.waybar = {
         cpu-temp-zone = 6;
       };
     };
-
-    system.kmonad = {
-      enable = true;
-      config =
-        # kbd
-        ''
-          (defcfg
-            input  (device-file "/dev/input/by-path/platform-i8042-serio-0-event-kbd")
-            output (uinput-sink "kmonad-laptop" "/run/current-system/sw/bin/sleep 1 && /run/current-system/sw/bin/setxkbmap -option compose:ralt")
-            cmp-seq ralt
-            cmp-seq-delay 5
-            fallthrough true
-            allow-cmd false
-          )
-
-          (defalias
-            caps (tap-hold 500 esc (layer-toggle layer1))
-          )
-
-          (defsrc
-            `       1       2       3       4       5       6       7       8       9       0       -       =       bspc
-            tab     q       w       e       r       t       y       u       i       o       p       [       ]       ret
-            caps    a       s       d       f       g       h       j       k       l       ;       '       lsgt
-            lsft    z       x       c       v       b       n       m       ,       .       /       rsft
-            lctl    lmet    lalt                    spc                     ralt            rctrl
-          )
-
-          (deflayer default
-            `       1       2       3       4       5       6       7       8       9       0       -       =       bspc
-            tab     q       w       e       r       t       y       u       i       o       p       [       ]       ret
-            @caps   a       s       d       f       g       h       j       k       l       ;       '       lsgt
-            lsft    z       x       c       v       b       n       m       ,       .       /       rsft
-            lctl    lmet    lalt                    spc                     ralt            rctrl
-          )
-
-          (deflayer layer1
-            _       _       _       _       _       _       _       _       _       _       _       _       _       _
-            _       _       _       _       _       _       _       _       _       _       _       _       _       _
-            _       _       _       _       _       _       left    down    up      right   _       _       _
-            _       _       _       _       _       _       _       _       _       _       _       _
-            _       _       _                       _                       _               _
-          )
-
-        '';
-    };
-  };
-
-  home-manager.users.${config.${namespace}.user.name}.${namespace} = {
     tools.ssh.user.config = {
       "github.com" = {
         hostname = "github.com";
