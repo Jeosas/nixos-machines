379d64753d66ff636ff531f7efaae163cdb01d0a
Jeosas <jeanbaptiste@wintergerst.fr>
2025-08-20 16:06:28 +0200

    chore(neovim): update renamed nvim-tree config

--- packages/neovim/ui/nvim-tree.nix
+++ packages/neovim/ui/nvim-tree.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ _, ... }:
 {
   keymaps = [
     {
@@ -15,78 +15,80 @@
   plugins.nvim-tree = {
     enable = true;
     autoClose = true;
-    hijackCursor = true;
-    respectBufCwd = true;
-    syncRootWithCwd = true;
-    updateFocusedFile = {
-      enable = true;
-      updateRoot = true;
-    };
-    diagnostics = {
-      enable = true;
-      showOnDirs = true;
-      icons = {
-        hint = "󱠂";
-        info = "";
-        warning = "";
-        error = "";
+    settings = {
+      hijack_cursor = true;
+      respect_buf_cwd = true;
+      sync_root_with_cwd = true;
+      update_focused_file = {
+        enable = true;
+        update_root = true;
       };
-    };
-    view = {
-      side = "left";
-      width = {
-        min = 30;
-        max = "25%";
+      diagnostics = {
+        enable = true;
+        show_on_dirs = true;
+        icons = {
+          hint = "󱠂";
+          info = "";
+          warning = "";
+          error = "";
+        };
       };
-    };
-    actions = {
-      openFile = {
-        quitOnOpen = true;
+      view = {
+        side = "left";
+        width = {
+          min = 30;
+          max = "25%";
+        };
       };
-    };
-    renderer = {
-      rootFolderLabel = ":t";
-      icons = {
-        glyphs = {
-          default = "";
-          symlink = "";
-          folder = {
-            arrowOpen = "";
-            arrowClosed = "";
-            default = "";
-            open = "";
-            empty = "";
-            emptyOpen = "";
-            symlink = "";
-            symlinkOpen = "";
-          };
-          git = {
-            unstaged = "";
-            staged = "S";
-            unmerged = "";
-            renamed = "➜";
-            untracked = "U";
-            deleted = "";
-            ignored = "◌";
+      actions = {
+        open_file = {
+          quit_on_open = true;
+        };
+      };
+      renderer = {
+        root_folder_label = ":t";
+        icons = {
+          glyphs = {
+            default = "";
+            symlink = "";
+            folder = {
+              arrow_open = "";
+              arrow_closed = "";
+              default = "";
+              open = "";
+              empty = "";
+              empty_open = "";
+              symlink = "";
+              symlink_open = "";
+            };
+            git = {
+              unstaged = "";
+              staged = "S";
+              unmerged = "";
+              renamed = "➜";
+              untracked = "U";
+              deleted = "";
+              ignored = "◌";
+            };
           };
         };
       };
+      on_attach.__raw =
+        #lua
+        ''
+          function(bufnr)
+          	local api = require("nvim-tree.api")
+
+          	local function opts(desc)
+          		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          	end
+
+          	api.config.mappings.default_on_attach(bufnr)
+
+          	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+          	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close"))
+          end
+        '';
     };
-    onAttach.__raw =
-      #lua
-      ''
-        function(bufnr)
-        	local api = require("nvim-tree.api")
-
-        	local function opts(desc)
-        		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        	end
-
-        	api.config.mappings.default_on_attach(bufnr)
-
-        	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
-        	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close"))
-        end
-      '';
   };
 }
