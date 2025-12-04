a4e9d68bce8aa452c0eeef595224ad9a80875d0d
Jeosas <jeanbaptiste@wintergerst.fr>
2025-12-04 14:47:02 +0100

    fix: allow bridges for libvirt

--- modules/apps/libvirtd.nix
+++ modules/apps/libvirtd.nix
@@ -24,11 +24,20 @@ in
         swtpm.enable = true;
         vhostUserPackages = [ pkgs.virtiofsd ];
       };
-      nss.enable = true;
+      nss = {
+        enable = true;
+        enableGuest = true;
+      };
       allowedBridges = [
         "virbr0"
       ];
     };
+
+    networking.firewall.trustedInterfaces = [
+      "virbr0"
+    ];
+
+    programs.virt-manager.enable = true;
 
     ${namespace} = {
       user.extraGroups = [ "libvirtd" ];
