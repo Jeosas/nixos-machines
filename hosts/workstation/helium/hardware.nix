1f47150fcf07e4f5cd36041a6c40e9766794119c
Jeosas <jeanbaptiste@wintergerst.fr>
2025-12-16 15:28:13 +0100

    feat: adjusted tlp settings on helium

--- hosts/workstation/helium/hardware.nix
+++ hosts/workstation/helium/hardware.nix
@@ -27,16 +27,6 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_BOOST_ON_AC = 1;
-        CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
-        CPU_DRIVER_OPMODE_ON_AC = "passive";
-        PLATFORM_PROFILE_ON_AC = "balanced";
-
-        CPU_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_BAT = "conservative";
-        CPU_DRIVER_OPMODE_ON_BAT = "passive";
-        PLATFORM_PROFILE_ON_BAT = "low-power";
-
         START_CHARGE_THRESH_BAT0 = 65;
         STOP_CHARGE_THRESH_BAT0 = 80;
       };
