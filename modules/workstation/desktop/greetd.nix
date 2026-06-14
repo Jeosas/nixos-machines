030c83f494dd4f426ea39802e45fbea11b7b4f22
Jeosas <jeanbaptiste@wintergerst.fr>
2026-06-14 10:56:17 +0200

    fix: systemd messages appearing on greetd tui

--- modules/workstation/desktop/greetd.nix
+++ modules/workstation/desktop/greetd.nix
@@ -1,9 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   config = {
     services.greetd = {
       enable = true;
       settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
+    };
+
+    # Fix systemd messages appearing on login tui
+    systemd.services.greetd = {
+      unitConfig = {
+        After = lib.mkOverride 0 [ "multi-user.target" ];
+      };
+      serviceConfig = {
+        Type = "idle";
+      };
     };
   };
 }
